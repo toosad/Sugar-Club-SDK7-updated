d1287a0e527c493a95b79a98954c48692aef1f56
SugarClub <50675840+toosad@users.noreply.github.com>
2025-05-13 11:18:18 +0200

    V1

---
+++ assets/scene/entity-names.ts
@@ -0,0 +1,14 @@
+// Auto-generated entity names from the scene
+
+
+/**
+ * Object containing all entity names in the scene for autocomplete support.
+ */
+export enum EntityNames {
+  Admin_Tools = "Admin Tools",
+  Audio_Stream = "Audio Stream",
+  Teleport_Main_dance_floor = "Teleport Main dance floor",
+  Teleport_Skylounge = "Teleport Skylounge",
+  Video_Screen = "Video Screen",
+  sc_2025_glb = "sc_2025.glb",
+} 
