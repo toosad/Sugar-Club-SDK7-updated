01e64c1882a58455d8890ac9eea06e59dfa225b8
SugarClub <50675840+toosad@users.noreply.github.com>
2025-05-31 11:52:51 +0200

    Update TextInfo + URL Wearshop + Club animations

--- assets/scene/entity-names.ts
+++ assets/scene/entity-names.ts
@@ -7,12 +7,13 @@
 export enum EntityNames {
   Admin_Tools = "Admin Tools",
   Audio_Stream = "Audio Stream",
-  Club_info = "Club info",
-  SugarClub_Wear_link = "SugarClub Wear link",
+  Discord_Button = "Discord Button",
   Teleport_Main_dance_floor = "Teleport Main dance floor",
   Teleport_Skylounge = "Teleport Skylounge",
-  Text_2 = "Text_2",
+  Text = "Text",
   Video_Screen = "Video Screen",
+  donut2_noarmature_glb = "donut2_noarmature.glb",
   mooxx_norig_glb = "mooxx_norig.glb",
   sc_2025_02_glb = "sc_2025_02.glb",
+  sugar_x_legend_display_glb = "sugar_x_legend_display.glb",
 } 
